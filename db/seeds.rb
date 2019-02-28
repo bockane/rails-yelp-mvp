01bf5f88220528ff0ae90d11637d1712ce2eda4a
Agnese Bockane <agnese.bockane@gmail.com>
2019-02-28 09:11:44 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    rating:        5
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    rating:        4
+  },
+  {
+    name:         'Roca',
+    address:      '31 Rue Guillaume Tell, 75017 Paris, France',
+    category:     'belgian',
+    rating:        4
+  },
+  {
+    name:         'Nobu',
+    address:      '15 Berkeley St, Mayfair, London W1J 8DY, UK',
+    category:     'japanese',
+    rating:        3
+  },
+   {
+    name:         'Anahi',
+    address:      '49 Rue Volta, 75003 Paris, France',
+    category:      'japanese',
+    rating:        5
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
